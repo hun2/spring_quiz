9d10b136fe431eacf12567fa0d0f947ddae87f87
DESKTOP-NMBASVR\user <rlgnswmfwmf@gmail.com>
2022-09-28 21:41:12 +0900

    20220928

---
+++ src/main/java/com/quiz/lesson05/Lesson05Controller.java
@@ -0,0 +1,16 @@
+package com.quiz.lesson05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Lesson05Controller {
+
+	
+	//요청 url :
+	@RequestMapping("/lesson5/quiz01")
+	public String quiz01() {
+		
+		return "lesson05/quiz01";
+	}
+}
